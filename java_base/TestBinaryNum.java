01ea862977c4aa3d38c49c6a820f04b3cfeefd56
abigbird <ningjinlei@qq.com>
2016-06-28 23:20:23 +0800

    20160629关注内容

--- java_base/TestBinaryNum.java
+++ java_base/TestBinaryNum.java
@@ -2,6 +2,8 @@ public class TestBinaryNum{
 	
 	//jdk1.7 新增特性：二进制表示形式，下划线分隔符表示形式
 	public static void main(String[] args){
+		System.out.println(System.getProperty("file.encoding"));
+		
 		int a = 0b00000000_00000000_00000000_00000011;
 		int b = 012; 
 		int c = 1_2098_1029;
