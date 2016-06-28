c757b66da0ffc07240bf7e14c02a104dbe7e6b02
abigbird <ningjinlei@qq.com>
2016-06-28 22:54:32 +0800

    java1.7新特性 二进制分隔符

---
+++ java_base/TestBinaryNum.java
@@ -0,0 +1,15 @@
+public class TestBinaryNum{
+	
+	//jdk1.7 新增特性：二进制表示形式，下划线分隔符表示形式
+	public static void main(String[] args){
+		int a = 0b00000000_00000000_00000000_00000011;
+		int b = 012; 
+		int c = 1_2098_1029;
+		int d = 0x32F;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+	
+}
